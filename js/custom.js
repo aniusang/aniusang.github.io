5b4b85d7e09fa2c58ba76954f0ecb6303ea84d27
niusang <niusang@niusang.cn>
2024-12-21 07:39:48 +0800

    Site updated: 2024-12-21 07:39:48

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var licenseBox = document.querySelector('.license-box');
+    if (licenseBox) {
+      licenseBox.style.display = 'none';
+    }
+  });
+  
